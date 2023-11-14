34843db1f258ff2b3310975f1b1e47b8a6a7fe88
sannyasannya <sanya.vasileva@mail.bg>
2023-11-14 16:01:09 +0200

    Add problems of "Conditional Statements"'s exercise.

---
+++ Programming_Basics_C#/VerificationLab/07.AreaOfFigures/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _07.AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            if (figure == "square")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double area = sideA * sideA;
+                Console.WriteLine($"{area:F3}");
+            }
+            else if (figure == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                double area = sideA * sideB;
+                Console.WriteLine($"{area:F3}");
+            }
+            else if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                double area = Math.PI * (Math.Pow(radius, 2));
+                Console.WriteLine($"{area:F3}");
+
+            }
+            else if (figure == "triangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double heightA= double.Parse(Console.ReadLine());
+                double area = sideA * heightA /2;
+                Console.WriteLine($"{area:F3}");
+            }
+        }
+    }
+}
