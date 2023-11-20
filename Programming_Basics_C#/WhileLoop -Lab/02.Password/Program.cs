c14cad5930c33aaf29c9518c3da390bbfa248998
sannyasannya <sanya.vasileva@mail.bg>
2023-11-20 15:35:35 +0200

    Add four problems from While Loop - Lab.

---
+++ Programming_Basics_C#/WhileLoop -Lab/02.Password/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02.Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            string password2 = Console.ReadLine();
+
+            while (password != password2)
+            {                
+                password2 = Console.ReadLine();
+            }
+            Console.WriteLine($"Welcome {username}!");         
+        }
+    }
+}
---
+++ Programming_Basics_C#/WhileLoop -Lab/03.SumNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int sum = 0;
+            
+            while (number1 > sum)
+            {
+                int number2 = int.Parse(Console.ReadLine());
+                sum += number2;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
