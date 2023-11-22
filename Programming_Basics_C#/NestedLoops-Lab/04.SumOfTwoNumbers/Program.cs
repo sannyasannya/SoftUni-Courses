c5171b13657a5c80c1b3f6720b4b0fd0cd6da55e
sannyasannya <sanya.vasileva@mail.bg>
2023-11-22 16:55:14 +0200

    Add all problems from Nested Loops - Lab.

---
+++ Programming_Basics_C#/NestedLoops-Lab/04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04.SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int counter = 0;
+            int combinationCounter = 0;
+
+            for (int x = num1; x <= num2; x++)
+            {
+                for (int y = num1; y <= num2; y++)
+                {
+                    combinationCounter++;
+
+                    if (x + y == magicNumber)
+                    {
+                        counter++;
+                        Console.WriteLine($"Combination N:{combinationCounter} ({x} + {y} = {magicNumber})");
+                        return;
+                    }
+                }
+            }
+
+            if (counter == 0)
+            {
+                Console.WriteLine($"{combinationCounter} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
---
+++ Programming_Basics_C#/NestedLoops-Lab/06.Building/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06.Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();            
+
+            while (destination != "End")
+            {
+                double budget = double.Parse(Console.ReadLine());
+                double sum = 0;
+                while (sum < budget)
+                {
+                    double currentSavedMoney = double.Parse(Console.ReadLine());
+                    
+                    sum += currentSavedMoney;
+                }
+                Console.WriteLine($"Going to {destination}!");
+
+                destination = Console.ReadLine();
+            }
+
+        }
+    }
+}
