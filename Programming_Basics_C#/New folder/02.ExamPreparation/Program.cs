bedcf3a283c72535fb915e0c97027ceab1e4adb4
sannyasannya <sanya.vasileva@mail.bg>
2023-11-25 13:10:17 +0200

    Add another problems into Programming Basics with C#.

---
+++ Programming_Basics_C#/New folder/02.ExamPreparation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02.ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int poorGrade = int.Parse(Console.ReadLine());
+
+            int sumGrade = 0;
+            int poorGradeCounter = 0;
+
+            string lastProblemsName = "";
+            string problemsName = Console.ReadLine();
+
+            while (problemsName != "Enough")
+            {                
+                int grade = int.Parse(problemsName);                
+
+                if (grade <= 4)
+                {
+                    poorGradeCounter++;
+                }
+                sumGrade += grade;
+
+                lastProblemsName = problemsName;
+                problemsName = Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ Programming_Basics_C#/New folder/03.Vacation/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _03.Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double neededMoney = double.Parse(Console.ReadLine());
+            double ownedMoney = double.Parse(Console.ReadLine());           
+            int daysCounter = 0;
+            int totalDays = 0;
+
+            while (daysCounter < 5 && ownedMoney < neededMoney)
+            {
+                string action = Console.ReadLine();
+                double money = double.Parse(Console.ReadLine());
+                
+                if (action == "save")
+                {
+                    ownedMoney += money;
+                    daysCounter = 0;
+                    
+                }
+                else 
+                {
+                    daysCounter++;
+                    ownedMoney -= money;
+
+                    if (money < 0)
+                    {
+                        money = 0;
+                    }
+                }
+                totalDays++;                
+
+
+                if (ownedMoney >= neededMoney)
+                {
+                    Console.WriteLine($"You saved the money for {totalDays} days.");
+                    break;
+                }
+
+                if (daysCounter == 5)
+                {
+                    Console.WriteLine($"You can't save the money.");
+                    Console.WriteLine($"{totalDays}");
+                    break;
+                   
+                }                
+            }
+          
+
+        }
+    }
+}
