d1608f1111762cc62fedff8e8a84e5c4d96b92f7
sannyasannya <sanya.vasileva@mail.bg>
2023-11-26 10:18:12 +0200

    Add problems from Programming Basics with C#.

---
+++ Programming_Basics_C#/NestedLoopsExercise/N2FamilyTripExam6July2019/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace N2FamilyTripExam6July2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal budget = decimal.Parse(Console.ReadLine());
+            int nights = int.Parse(Console.ReadLine());
+            decimal nightPrice = decimal.Parse(Console.ReadLine());
+            int percentAddExp = int.Parse(Console.ReadLine());
+
+
+            decimal totalNight = nights * nightPrice;
+
+            if (nights > 7)
+            {
+                totalNight = totalNight - 0.05m * totalNight;
+            }
+            
+            
+
+            decimal AddPrice = (budget * percentAddExp) / 100;
+            decimal totalExp = AddPrice + totalNight;
+            decimal remainingMoney = budget - totalExp;
+            decimal requiredMoney = totalExp - budget;
+            
+
+            if (budget >= totalExp )
+            {
+                Console.WriteLine($"Ivanovi will be left with {remainingMoney:f2} leva after vacation.");
+            }
+            else
+            {
+                Console.WriteLine($"{requiredMoney:f2} leva needed.");
+            }
+
+        }
+    }
+}
---
+++ Programming_Basics_C#/New folder/06.Cake/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06.Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int totalPieces = lenght * width;
+
+            string input = Console.ReadLine();
+
+            while (input != "STOP")
+            {
+                int currentPiece = int.Parse(input);
+
+                if (currentPiece > totalPieces)
+                {
+                    double requiredPieces = currentPiece - totalPieces;
+                    Console.WriteLine($"No more cake left! You need {requiredPieces} pieces more.");
+                    return;
+                    
+                }
+                else
+                {
+                    totalPieces -= currentPiece;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{totalPieces} pieces are left.");
+        }
+    }
+}
