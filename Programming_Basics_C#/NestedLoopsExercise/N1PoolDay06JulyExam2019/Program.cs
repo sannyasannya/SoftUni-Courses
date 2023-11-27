5ea1c53823ab2a923d478db47d4e117701bd2b0f
sannyasannya <sanya.vasileva@mail.bg>
2023-11-27 11:29:39 +0200

    Add a new problem from Programming Basic with C#.

---
+++ Programming_Basics_C#/NestedLoopsExercise/N1PoolDay06JulyExam2019/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace N1PoolDay06JulyExam2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            double entrance = double.Parse(Console.ReadLine());
+            double sunLongerPrice = double.Parse(Console.ReadLine());
+            double umbrellaPrice = double.Parse(Console.ReadLine());
+
+            double sumPrice = people * entrance;
+            double peopleForLonger = Math.Ceiling(0.75 * people);
+            double totalLongerPrice = peopleForLonger * sunLongerPrice;
+            double umbrellaForPeople = Math.Ceiling(0.5 * people);
+            double totalUmbrellaPrice = umbrellaForPeople *umbrellaPrice;
+            double totalPrice = sumPrice + totalLongerPrice + totalUmbrellaPrice;
+
+            Console.WriteLine($"{totalPrice:f2} lv.");
+
+        }
+    }
+}
