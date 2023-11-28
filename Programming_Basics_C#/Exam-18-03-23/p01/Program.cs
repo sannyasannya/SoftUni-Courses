4022e5d85912ed107a9734fb885e25a9a4b0bdab
sannyasannya <sanya.vasileva@mail.bg>
2023-11-28 10:25:04 +0200

    Add a new problem from C# - Programming Basics Course.

---
+++ Programming_Basics_C#/Exam-18-03-23/p01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace p01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int paperCount = int.Parse(Console.ReadLine());
+            int clothCount = int.Parse(Console.ReadLine());
+            double glueLiter = double.Parse(Console.ReadLine());
+            int percentDiscount = int.Parse(Console.ReadLine());
+
+            double paperPrice = paperCount * 5.8;
+            double clothPrice = clothCount * 7.2;
+            double gluePrice = glueLiter * 1.2;
+
+            double materialsPrice = paperPrice + clothPrice + gluePrice;
+            double priceDiscount = materialsPrice - (percentDiscount * materialsPrice / 100);
+
+            Console.WriteLine($"{priceDiscount:f3}");
+        }
+    }
+}
