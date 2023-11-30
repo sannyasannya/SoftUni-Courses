969399a9ca62438f88777be0433723465da26fd4
sannyasannya <sanya.vasileva@mail.bg>
2023-12-01 00:27:24 +0200

    Add new problem from C# Basics.

---
+++ Programming_Basics_C#/Exam-18-03-23/p04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace p04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double startingKm = double.Parse(Console.ReadLine());
+            double kmCounter = startingKm;
+
+            for (int i = 0; i < days; i++)
+            {
+                int percent = int.Parse(Console.ReadLine());
+                startingKm = startingKm +((percent * startingKm) / 100);
+                kmCounter += startingKm;
+            }
+
+            if (kmCounter >= 1000)
+            {
+                double moreKm = Math.Ceiling(kmCounter - 1000);
+                Console.WriteLine($"You've done a great job running {moreKm} more kilometers!");
+            }
+            else
+            {
+                double requiredKm = Math.Ceiling(1000 - kmCounter);
+                Console.WriteLine($"Sorry Mrs. Ivanova, you need to run {requiredKm} more kilometers");
+            }
+        }
+    }
+}
