4725855276e5b49e84d66bf0be4e98d199fb38f0
sannyasannya <sanya.vasileva@mail.bg>
2023-12-01 00:28:11 +0200

    Add another problem from Programming Basics with C#.

---
+++ Programming_Basics_C#/Exam-18-03-23/p05/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace p05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int target = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            double price = 0;
+
+            while (command != "closed")
+            {
+                if (command == "haircut")
+                {
+                    string haircutType = Console.ReadLine();
+                    if (haircutType == "mens")
+                    {
+                        price += 15;
+                    }
+                    else if (haircutType == "ladies")
+                    {
+                        price += 20;
+                    }
+                    else if (haircutType == "kids")
+                    {
+                        price += 10;
+                    }
+                }
+                else if (command == "color")
+                {
+                    string colorType = Console.ReadLine();
+                    if (colorType == "touch up")
+                    {
+                        price += 20;
+                    }
+                    else if (colorType == "full color")
+                    {
+                        price += 30;
+                    }
+                }
+
+                if (price >= target)
+                {
+                    break;
+                }
+                command = Console.ReadLine();
+            }
+
+            if (price >= target)
+            {
+                
+                Console.WriteLine("You have reached your target for the day!");
+                Console.WriteLine($"Earned money: {price}lv.");
+            }
+            else
+            {
+                double requiredMoney = target - price;
+                Console.WriteLine($"Target not reached! You need {requiredMoney}lv. more.");
+                Console.WriteLine($"Earned money: {price}lv.");
+            }
+        }
+    }
+}
