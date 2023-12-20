2fa980d6ee7ffe5e6287ca23cc14145bd8bb98f2
sannyasannya <sanya.vasileva@mail.bg>
2023-12-20 10:28:40 +0200

    Add new problems from Course: Programming Fundamentals and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/ConsoleApp1/08.TheGreaterNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _08.TheGreaterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("Greater number: " + a);
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("Greater number: " + b);
+            }
+            else
+            {
+                Console.WriteLine("The two numbers are equal.");
+            }
+        }
+    }
+}
