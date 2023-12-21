2397d72c629148396d2081565574246df7b2bcc8
sannyasannya <sanya.vasileva@mail.bg>
2023-12-21 11:09:03 +0200

    Add new problems from While and For Loops, and Nested Loops.

---
+++ Programming_Fundamentals_And_Unit_Testing/WhileAndForLoopsNestedLoops-LAB/05.SumOfNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05.SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (sum < maxNumber)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                sum += currentNumber;
+            }
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
---
+++ Programming_Fundamentals_And_Unit_Testing/WhileAndForLoopsNestedLoops-LAB/WhileAndForLoopsNestedLoops-LAB/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WhileAndForLoopsNestedLoops_LAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(i + 1);
+            }
+        }
+    }
+}
