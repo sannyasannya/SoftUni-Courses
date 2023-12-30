a138f7393d446b46d07534a6d9a6762f75ab407b
sannyasannya <sanya.vasileva@mail.bg>
2023-12-30 17:42:40 +0200

    Add new problems from Methods.

---
+++ Programming_Fundamentals_And_Unit_Testing/Methods-LAB/08.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _08.MultiplyEvensByOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int oddSum = 0;
+            int evenSum = 0;
+
+            int num = int.Parse(number);
+            num = Math.Abs(num);
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                int lastDigit = num % 10;
+                num = num / 10;
+                evenSum = GetSumOfEvenDigits(evenSum, lastDigit);
+
+                oddSum = GetSumOfOddDigits(oddSum, lastDigit);
+
+            }
+            int totaSum = GetMultipleOfEvenAndOdds(oddSum, evenSum);
+            Console.WriteLine(totaSum);
+
+        }
+
+        private static int GetMultipleOfEvenAndOdds(int oddSum, int evenSum)
+        {
+            return Math.Abs(oddSum * evenSum);
+        }
+
+        private static int GetSumOfOddDigits(int oddSum, int lastDigit)
+        {
+            if (lastDigit % 2 == 1)
+            {
+                oddSum += lastDigit;
+            }
+
+            return oddSum;
+        }
+
+        private static int GetSumOfEvenDigits(int evenSum, int lastDigit)
+        {
+            if (lastDigit % 2 == 0)
+            {
+                evenSum += lastDigit;
+            }
+
+            return evenSum;
+        }
+    }
+}
---
+++ Programming_Fundamentals_And_Unit_Testing/Methods-LAB/09.Orders/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _09.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            double result = Order(product, quantity);
+            Console.WriteLine($"{result:f2}");
+        }
+
+        static double Order(string product, int quantity)
+        {
+            double price = 0;
+
+            if (product == "coffee")
+            {
+                price = 1.50;
+            }
+            else if (product == "water")
+            {
+                price = 1.00;
+            }
+            else if (product == "coke")
+            {
+                price = 1.40;
+            }
+            else if (product == "snacks")
+            {
+                price = 2.00;
+            }
+
+            double sum = price * quantity;
+            return sum;
+        }
+    }
+}
