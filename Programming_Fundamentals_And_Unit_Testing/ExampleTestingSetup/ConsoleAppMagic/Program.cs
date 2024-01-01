1122fea41c137a66f270eacb0f17f4a9e3f86155
sannyasannya <sanya.vasileva@mail.bg>
2024-01-01 16:20:25 +0200

    Add a new problem from Programming Fundamentals and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/ExampleTestingSetup/ConsoleAppMagic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleAppMagic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = Sum(10, 20);
+
+            Console.WriteLine();
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
