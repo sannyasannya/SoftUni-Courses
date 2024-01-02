aeb55a4afa9d4bfc01e94f65f82a7c45989448c3
sannyasannya <sanya.vasileva@mail.bg>
2024-01-02 12:51:13 +0200

    Add new problem from Programing Fundamentals and Unit Testing,

---
+++ Programming_Fundamentals_And_Unit_Testing/ProgrammingFundamentalsAndUnitTestingExams/Problem-1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Problem_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            double sumGrade = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                sumGrade += grade;
+            }             
+
+            double averageGrade = sumGrade / num;
+            Console.WriteLine($"{averageGrade:f2}");
+        }
+    }
+}
