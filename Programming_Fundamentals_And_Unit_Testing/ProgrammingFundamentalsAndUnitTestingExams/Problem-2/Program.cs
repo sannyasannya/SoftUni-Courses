51006dd6d07bbcdc4836120788b7b2e16bf66a57
sannyasannya <sanya.vasileva@mail.bg>
2024-01-03 12:50:10 +0200

    Add new problem from Programming Fundamentals and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/ProgrammingFundamentalsAndUnitTestingExams/Problem-2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Problem_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                              .Split(" ")
+                              .Select(int.Parse)
+                              .ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int lastNum = input[input.Count - 1];
+                input.RemoveAt(input.Count - 1);
+                input.Insert(0, lastNum);
+
+            }
+            Console.WriteLine(string.Join(", ", input).Trim());
+        }
+    }
+}
