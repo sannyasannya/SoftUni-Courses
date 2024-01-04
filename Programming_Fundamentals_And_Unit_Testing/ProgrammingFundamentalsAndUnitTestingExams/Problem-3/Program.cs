f3218b93f38d8ddf13684085b364dc05af364af0
sannyasannya <sanya.vasileva@mail.bg>
2024-01-04 19:47:24 +0200

    Add new problem from Programming and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/ProgrammingFundamentalsAndUnitTestingExams/Problem-3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Problem_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] result = new[] { "{", "{" };
+            Console.WriteLine(BalancedBrackets.IsBalanced(result));
+        }
+
+    }
+    public class BalancedBrackets
+    {
+        public static bool IsBalanced(string[] input)
+        {
+            int balance = 0;
+
+            foreach (string symbol in input)
+            {
+                if (symbol == "(")
+                {
+                    balance++;
+                }
+                else if (symbol == ")")
+                {
+                    balance--;
+                }
+
+                if (balance < 0)
+                {
+                    return false;
+                }
+            }
+
+            return balance == 0;
+        }
+    }
+
+}
