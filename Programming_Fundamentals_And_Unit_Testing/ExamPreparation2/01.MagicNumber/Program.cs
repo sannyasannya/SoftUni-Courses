b25e74d73d61b00cdbe40123d5a89677cedc5664
sannyasannya <sanya.vasileva@mail.bg>
2024-01-06 18:49:10 +0200

    Add a new problem from Programming Fundamentals and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/ExamPreparation2/01.MagicNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _01.MagicNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1;  i <= n; i++)
+            {
+                bool isPrime = true;
+                int divider = 2;
+                
+                while (divider < n) 
+                {
+                    if (i == divider)
+                    {
+                        divider++;
+                        continue;
+                    }
+
+                    if (i % divider == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                    divider++;
+                }
+                if (isPrime)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+
+
+            
+
+        }
+    }
+}
