0638be4d4a311bb2da759517c68ccd7a83a3ab54
sannyasannya <sanya.vasileva@mail.bg>
2024-01-07 20:31:38 +0200

    Add a new problem from Programming Fundamentals and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/ExamPreparation2/02.MinMaxValues/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.MinMaxValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<int> input = Console.ReadLine()
+                                .Split(" ")
+                                .Select(int.Parse)
+                                .ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            int maxNum = int.MinValue;
+            int minNum = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = input[i];
+
+                if (currentNum > maxNum)
+                {
+                    maxNum = currentNum;
+                }
+
+                if (currentNum < minNum)
+                {
+                    minNum = currentNum;
+                }
+            }
+            Console.WriteLine($"{maxNum}");
+            Console.WriteLine($"{minNum}");
+        }
+    }
+}
