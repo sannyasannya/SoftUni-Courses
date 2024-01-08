5b27449ca11a22cd04c9d0e3333748412550dde8
sannyasannya <sanya.vasileva@mail.bg>
2024-01-08 13:11:13 +0200

    Add a new problem from Programming Fundamentals and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/ExamPreparation2/2.MinMax2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _2.MinMax2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine()
+                                 .Split(" ")
+                                 .Select(int.Parse)
+                                 .ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> secondList = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = firstList[i];
+                secondList.Add(currentNum);
+            }
+            Console.WriteLine(secondList.Max());
+            Console.WriteLine(secondList.Min());
+
+        }
+    }
+}
