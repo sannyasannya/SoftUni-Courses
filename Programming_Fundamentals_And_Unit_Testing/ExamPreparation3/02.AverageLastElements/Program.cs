8f6eef493e47b8a843be82c05521c0b2542a5439
sannyasannya <sanya.vasileva@mail.bg>
2024-01-11 09:46:40 +0200

    Add a new problem from Programming Fundamentals and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/ExamPreparation3/02.AverageLastElements/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _02.AverageLastElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                             .Split(" ")
+                             .Select(int.Parse)
+                             .ToList();
+
+            int num = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            List<int> output = new List<int>();
+
+            for (int i = list.Count - 1; i > list.Count - 1 - num; i--)
+            {
+                int currentNum = list[i];
+                sum += currentNum;
+            }
+
+            double average = sum / num;
+
+            Console.WriteLine($"{average:f2}");
+        }
+    }
+}
