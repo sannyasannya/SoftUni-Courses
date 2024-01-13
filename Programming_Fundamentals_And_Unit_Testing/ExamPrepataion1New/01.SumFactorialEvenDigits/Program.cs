46c0c8518456be27e7ce49982a1cb09887819f85
sannyasannya <sanya.vasileva@mail.bg>
2024-01-13 10:47:42 +0200

    Add a new problem from Programming Fundamentals and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/ExamPrepataion1New/01.SumFactorialEvenDigits/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01.SumFactorialEvenDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int result = int.Parse(Console.ReadLine());
+
+            int sumOfFactorial = 0;
+
+            while (result != 0)
+            {
+                int digit = result % 10;
+
+                if (digit % 2 == 0)
+                {
+                    int resultFactorial = CalculateFactorial(digit);
+                    sumOfFactorial += resultFactorial;
+                }
+                result = result / 10;
+            }
+            Console.WriteLine(sumOfFactorial);
+        }
+
+        static int CalculateFactorial(int number)
+        {
+            int result = 1;
+
+            while (number > 0)
+            {
+                result = result * number;
+                number = number - 1;
+            }
+            return result;
+        }
+    }
+}
