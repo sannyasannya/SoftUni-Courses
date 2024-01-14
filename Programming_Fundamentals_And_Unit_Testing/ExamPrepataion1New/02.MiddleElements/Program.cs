5249a0d86e6cabde7809a9e3e2e20fe49dc7f60c
sannyasannya <sanya.vasileva@mail.bg>
2024-01-14 15:11:51 +0200

    Add another problem from Programming Fundamentals and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/ExamPrepataion1New/02.MiddleElements/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _02.MiddleElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                              .Split(" ")
+                              .Select(int.Parse)
+                              .ToArray();
+
+            int middleRightIndex = numbers.Length / 2;
+            int middleLeftIndex = middleRightIndex - 1;
+
+            int middleRightElement = numbers[middleRightIndex];
+            int middleLeftElement = numbers[middleLeftIndex];
+
+            double middleElementAverage = (middleRightElement + middleLeftElement) / 2.0;
+
+            Console.WriteLine($"{middleElementAverage:f2}");
+
+
+
+        }
+    }
+}
