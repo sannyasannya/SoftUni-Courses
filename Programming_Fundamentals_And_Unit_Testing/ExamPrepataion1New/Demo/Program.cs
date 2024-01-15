afa3efd55cc1334b97a345bfbbd7296b74866e0c
sannyasannya <sanya.vasileva@mail.bg>
2024-01-15 22:39:03 +0200

    Add a demo from Programming Fundamentals and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/ExamPrepataion1New/Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            printText("C#");
+        }
+
+        static void printText(string text)
+        {
+            Console.WriteLine("I love" + text);
+        }
+    }
+}
