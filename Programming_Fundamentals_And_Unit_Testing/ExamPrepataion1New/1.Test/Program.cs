b1b58290acf6a73d2b99653f5bfc27fd6abe5bd7
sannyasannya <sanya.vasileva@mail.bg>
2024-01-16 13:23:40 +0200

    Add a new problem from Programming Fundamentals and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/ExamPrepataion1New/1.Test/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _1.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int totalSum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string numAsString = input[i].ToString();
+                int num = int.Parse(numAsString);
+
+                if (num % 2 == 0)
+                {
+                    int resultFactorial = CalculateFactorial(num);
+                    totalSum += resultFactorial;
+                }
+            }
+            Console.WriteLine(totalSum);
+        }
+
+        static int CalculateFactorial(int number)
+        {
+            int result = 1;
+
+            while (number > 0)
+            {
+                result = result * number;
+                number = number - 1;
+            }
+            return result;
+        }
+    }
+}
