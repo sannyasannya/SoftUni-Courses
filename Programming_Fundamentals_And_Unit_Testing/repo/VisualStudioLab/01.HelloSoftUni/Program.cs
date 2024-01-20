f17252d7b1155b13bef11ce5952fa1e56c08ea92
sannyasannya <sanya.vasileva@mail.bg>
2024-01-20 09:20:29 +0200

    Add simple problems from Programming Fundamentals and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/repo/VisualStudioLab/01.HelloSoftUni/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01.HelloSoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, SoftUni");
+        }
+    }
+}
---
+++ Programming_Fundamentals_And_Unit_Testing/repo/VisualStudioLab/02.Greeting/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _02.Greeting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
