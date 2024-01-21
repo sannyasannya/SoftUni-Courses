7fe5cc75b80fdbcc54483c23737d7fab7f28bfd9
sannyasannya <sanya.vasileva@mail.bg>
2024-01-21 09:23:22 +0200

    Add new problems from Programming Fundamentals and Unit Testing.

---
+++ Programming_Fundamentals_And_Unit_Testing/repo/VisualStudioLab/0.5.TownInfo/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace _0._5.TownInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            string country = Console.ReadLine();
+
+            Console.WriteLine($"{town} is in {country}");
+        }
+    }
+}
