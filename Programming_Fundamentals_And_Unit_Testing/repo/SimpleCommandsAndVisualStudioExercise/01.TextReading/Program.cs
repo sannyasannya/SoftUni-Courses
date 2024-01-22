d7e8f08bf99c22e6f69f19d27d8f582f0b5ee2ea
sannyasannya <sanya.vasileva@mail.bg>
2024-01-22 16:57:42 +0200

    Add new problems from Exercise"Simple Commands and Visual Studio".

---
+++ Programming_Fundamentals_And_Unit_Testing/repo/SimpleCommandsAndVisualStudioExercise/01.TextReading/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _01.TextReading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ Programming_Fundamentals_And_Unit_Testing/repo/SimpleCommandsAndVisualStudioExercise/02.SquareArea/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _02.SquareArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int side = int.Parse(Console.ReadLine());
+            int area = side * side;
+
+            Console.WriteLine(area);
+        }
+    }
+}
