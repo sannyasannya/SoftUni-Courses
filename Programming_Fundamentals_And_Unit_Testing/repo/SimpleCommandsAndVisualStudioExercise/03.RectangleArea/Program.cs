cc16e035830b5d1fc7ac4322a57b9a73ae1f46ea
sannyasannya <sanya.vasileva@mail.bg>
2024-01-23 08:42:58 +0200

    Add new problems from Exercise"Simple Commands and Visual Studio".

---
+++ Programming_Fundamentals_And_Unit_Testing/repo/SimpleCommandsAndVisualStudioExercise/03.RectangleArea/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _03.RectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int widht = int.Parse(Console.ReadLine());
+
+            int area = lenght * widht;
+
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ Programming_Fundamentals_And_Unit_Testing/repo/SimpleCommandsAndVisualStudioExercise/04.TrapezoidArea/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _04.TrapezoidArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstBase = int.Parse(Console.ReadLine());
+            int secondBase = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int area = (firstBase + secondBase) / 2 * height;
+
+            Console.WriteLine(area);
+        }
+    }
+}
