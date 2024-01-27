d2f65e07579a1de4a24590f0f3f47e206347c065
sannyasannya <sanya.vasileva@mail.bg>
2024-01-27 07:48:11 +0200

    Add new problems from Programming Fundamentals and Unit Testing.

---
+++ Programming Fundamentals and Unit Testing - септември 2023/repo/DataTypesAndVariables/01.DayInMinutes/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _01.DayInMinutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+            int minutes = day * 24 * 60;
+
+            Console.WriteLine($"Minutes = {minutes}");
+        }
+    }
+}
---
+++ Programming Fundamentals and Unit Testing - септември 2023/repo/DataTypesAndVariables/02.CalculateSpeed/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _02.CalculateSpeed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double distance = double.Parse(Console.ReadLine());
+            double time = double.Parse(Console.ReadLine());
+
+            double speed = distance / time;
+
+            Console.WriteLine($"{speed:f2}");
+        }
+    }
+}
