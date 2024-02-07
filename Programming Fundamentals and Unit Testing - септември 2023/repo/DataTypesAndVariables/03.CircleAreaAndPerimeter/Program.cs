4141401ec77cb8664ca2dc6c27d68ae8e9e16e21
sannyasannya <sanya.vasileva@mail.bg>
2024-02-07 08:38:51 +0200

    Add a simple problem from Data Types and Variables.

---
+++ Programming Fundamentals and Unit Testing - септември 2023/repo/DataTypesAndVariables/03.CircleAreaAndPerimeter/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _03.CircleAreaAndPerimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radiusOfCircle = double.Parse(Console.ReadLine());
+            double area = radiusOfCircle * radiusOfCircle * Math.PI;
+            double perimeter = 2 * Math.PI * radiusOfCircle;
+
+            Console.WriteLine($"Area = {area:f2}");
+            Console.WriteLine($"Perimeter = {perimeter:f2}");
+        }
+    }
+}
