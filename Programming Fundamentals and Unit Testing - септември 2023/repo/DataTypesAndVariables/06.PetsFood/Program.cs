f05602711598173a0daee4e232d92c9597cc6b33
sannyasannya <sanya.vasileva@mail.bg>
2024-02-09 09:21:57 +0200

    Add new problems from Data Types and Variables.

---
+++ Programming Fundamentals and Unit Testing - септември 2023/repo/DataTypesAndVariables/06.PetsFood/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _06.PetsFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFood = int.Parse(Console.ReadLine());
+            int catFood = int.Parse(Console.ReadLine());
+
+            double dogFoodPrice = dogFood * 2.5;
+            double catFoodPrice = catFood * 4;
+            double totalPrice = dogFoodPrice + catFoodPrice;
+
+            Console.WriteLine($"{totalPrice:f2} lv.");
+        }
+    }
+}
---
+++ Programming Fundamentals and Unit Testing - септември 2023/repo/DataTypesAndVariables/07.ProjectsCreation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _07.ProjectsCreation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int countOfProjects = int.Parse(Console.ReadLine());
+            int neededHours = countOfProjects * 3;
+
+            Console.WriteLine($"The architect {name} will need {neededHours} hours to complete {countOfProjects} project/s.");
+        }
+    }
+}
