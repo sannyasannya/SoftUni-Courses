f32ff62945e860c6ef36a77d8564518d9bba285d
sannyasannya <sanya.vasileva@mail.bg>
2024-02-10 15:25:39 +0200

    Add new problems from Data Variables and Simple Calculations.

---
+++ Programming Fundamentals and Unit Testing - септември 2023/repo/DataTypesVariablesAndSimpleCalculations/01.ReversedNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.ReversedNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double num3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(num3);
+            Console.WriteLine(num2);
+            Console.WriteLine(num1);
+
+        }
+    }
+}
---
+++ Programming Fundamentals and Unit Testing - септември 2023/repo/DataTypesVariablesAndSimpleCalculations/02.CenturiesToMinutes/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _02.CenturiesToMinutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+
+            int years = centuries * 100;
+            double days = Math.Floor(years * 365.2422);
+            double hours = days * 24;
+            double minutes = hours * 60;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+
+        }
+    }
+}
