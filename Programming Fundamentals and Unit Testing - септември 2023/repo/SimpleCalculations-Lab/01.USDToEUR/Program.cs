15f8467463a40cf3976c7503d702ff544ccd2415
sannyasannya <sanya.vasileva@mail.bg>
2024-02-12 09:29:17 +0200

    Add simple problems from Programming Fundamentals and Unit Testing.

---
+++ Programming Fundamentals and Unit Testing - септември 2023/repo/SimpleCalculations-Lab/01.USDToEUR/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _01.USDToEUR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double euro = usd * 0.88;
+
+            Console.WriteLine($"{euro:f2}");
+        }
+    }
+}
---
+++ Programming Fundamentals and Unit Testing - септември 2023/repo/SimpleCalculations-Lab/02.FourOperations/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.FourOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+
+            double adding = num1 + num2;
+            double substraction = num1 - num2;
+            double multiplication = num1 * num2;
+            double division = num1 / num2;
+
+            Console.WriteLine($"{num1:f2} + {num2:f2} = {adding:f2}");
+            Console.WriteLine($"{num1:f2} - {num2:f2} = {substraction:f2}");
+            Console.WriteLine($"{num1:f2} * {num2:f2} = {multiplication:f2}");
+            Console.WriteLine($"{num1:f2} / {num2:f2} = {division:f2}");
+        }
+    }
+}
