2663024663ffbb1ea5405d2c79beb55a45d9554c
sannyasannya <sanya.vasileva@mail.bg>
2024-02-13 09:30:03 +0200

    Add new problems from Programming Fundamentals and Unit Testing Course.

---
+++ Programming Fundamentals and Unit Testing - септември 2023/repo/SimpleCalculations-Lab/03.Market/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03.Market
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tomatoPrice = double.Parse(Console.ReadLine());
+            double tomatoQuantity = double.Parse(Console.ReadLine());
+            double cucumberPrice = double.Parse(Console.ReadLine());
+            double cucumberQuantity = double.Parse(Console.ReadLine());
+
+            double totalTomatoPrice = tomatoPrice * tomatoQuantity;
+            double totalCucumberPrice = cucumberPrice * cucumberQuantity;
+
+            double totalPrice = totalTomatoPrice + totalCucumberPrice;
+
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+    }
+}
---
+++ Programming Fundamentals and Unit Testing - септември 2023/repo/SimpleCalculations-Lab/04.Tiles/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04.Tiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double bathroomWidth = double.Parse(Console.ReadLine());
+            double bathroomHeight = double.Parse(Console.ReadLine());
+            double tileWidth = double.Parse(Console.ReadLine());
+            double tileHeight = double.Parse(Console.ReadLine());
+
+            double bathroomArea = bathroomWidth * bathroomHeight;
+            double tileSurplus = bathroomArea + (0.1 * bathroomArea);
+            double tileArea = tileWidth * tileHeight;
+
+            double tilesNeeded = Math.Round(tileSurplus / tileArea);
+
+            Console.WriteLine($"{tilesNeeded}");
+        }
+    }
+}
