551efb6a781cdebe35885c9828c784b9fef63cd4
sannyasannya <sanya.vasileva@mail.bg>
2024-02-14 09:04:41 +0200

    Add a new problem from Programming Fundamentals and Unit Testing.

---
+++ Programming Fundamentals and Unit Testing - септември 2023/repo/SimpleCalculations-Lab/05.DepositCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _05.DepositCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal depositAmount = decimal.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            decimal annualInterestRate = decimal.Parse(Console.ReadLine());
+
+            decimal accumulatedInterest = depositAmount * (annualInterestRate / 100);
+            decimal interestForOneMonth = accumulatedInterest / 12;
+            decimal totalAmount = depositAmount + (months * interestForOneMonth);
+
+            Console.WriteLine($"{totalAmount:f2}");
+
+        }
+    }
+}
