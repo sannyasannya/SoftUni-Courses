54db79e753fe30faa2d18bdfa09f38731884ea8c
sannyasannya <sanya.vasileva@mail.bg>
2024-02-15 16:39:56 +0200

    Add a new problem from programming Fundamentals and Unit Testing.

---
+++ Programming Fundamentals and Unit Testing - септември 2023/repo/SimpleCalculations-Lab/06.MandatoryLiterature/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _06.MandatoryLiterature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pages = int.Parse(Console.ReadLine());
+            int pagesForHour = int.Parse(Console.ReadLine());
+            int numberOfDay = int.Parse(Console.ReadLine());
+
+            int readingTime = pages / pagesForHour;
+            int requiredHours = readingTime / numberOfDay;
+
+            Console.WriteLine(requiredHours);
+        }
+    }
+}
