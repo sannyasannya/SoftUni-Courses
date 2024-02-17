c4e3b166169046d4813edbb23855c1fb8c7da1fc
sannyasannya <sanya.vasileva@mail.bg>
2024-02-17 17:56:10 +0200

    Add new problems from Strings and Test Processing.
    (Programming Advanced for QA Course)

---
+++ Programming Advanced for QA - ноември 2023/repos/StringsAndTextProcessingLab/02.RepeatStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02.RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ");
+
+            string output = string.Empty;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string currentWord = words[i];
+
+                for (int j = 0; j < currentWord.Length; j++)
+                {
+                    output += currentWord;
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
