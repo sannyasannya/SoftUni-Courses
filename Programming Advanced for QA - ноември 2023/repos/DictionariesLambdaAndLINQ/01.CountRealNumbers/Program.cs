1d43f28ce8b73e3b8bb889fbf7c40fad91d09c6f
sannyasannya <sanya.vasileva@mail.bg>
2024-02-21 16:34:54 +0200

    Add a new problems from Dictionaries, Lambda and LINQ.

---
+++ Programming Advanced for QA - ноември 2023/repos/DictionariesLambdaAndLINQ/01.CountRealNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            SortedDictionary<int, int> num = new();
+
+            foreach (int number in numbers)
+            {
+                if (num.ContainsKey(number))
+                {
+                    num[number] += 1;
+                }
+                else
+                {
+                    num.Add(number, 1);
+                    //num[numbers] = 1;
+                }
+            }
+
+            foreach (KeyValuePair<int, int> pair in num) 
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }          
+
+        }
+    }
+}
---
+++ Programming Advanced for QA - ноември 2023/repos/DictionariesLambdaAndLINQ/02.OddOccurrences/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ");
+
+            Dictionary<string, int> wordsCount = new();
+
+            foreach (var word in words)
+            {
+                string caseInsensitive = word.ToLower();
+
+                if (wordsCount.ContainsKey(caseInsensitive))
+                {
+                    wordsCount[caseInsensitive] += 1;
+                }
+                else
+                {
+                    wordsCount.Add(caseInsensitive, 1);
+                }
+            }
+
+            foreach (KeyValuePair<string, int> pair in wordsCount)
+            {
+                if (pair.Value % 2 != 0)
+                {
+                    Console.Write($"{pair.Key} ");
+                }
+            }
+
+        }
+    }
+}
