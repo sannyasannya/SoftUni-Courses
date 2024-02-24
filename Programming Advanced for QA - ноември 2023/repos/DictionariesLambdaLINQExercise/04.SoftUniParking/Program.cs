efc766b4b3ad5605d8fb22e258b37c01f0328df9
sannyasannya <sanya.vasileva@mail.bg>
2024-02-24 15:21:21 +0200

    Add new problems from Dictionaries, Lambda and LINQ.

---
+++ Programming Advanced for QA - ноември 2023/repos/DictionariesLambdaLINQExercise/04.SoftUniParking/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _04.SoftUniParking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> registrars = new();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] commandArray = Console.ReadLine().Split().ToArray();
+
+                string command = commandArray[0];
+                string employee = commandArray[1];
+
+                if (command == "register")
+                {
+                    string plateNumber = commandArray[2];
+
+                    if (registrars.ContainsKey(employee)) 
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {registrars[employee]}");
+                    }
+                    else
+                    {
+                        registrars.Add(employee, plateNumber);
+                        Console.WriteLine($"{employee} registered {plateNumber} successfully");
+                    }
+                }
+                else if (command == "unregister")
+                {                   
+
+                    if (registrars.ContainsKey(employee))
+                    {
+                        registrars.Remove(employee);
+                        Console.WriteLine($"{employee} unregistered successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: user {employee} not found");
+                    }
+                }
+            }
+            foreach (KeyValuePair<string, string> pair in registrars)
+            {
+                Console.WriteLine($"{pair.Key} => {pair.Value}");
+            }
+
+        }
+            
+
+    }
+}
---
+++ Programming Advanced for QA - ноември 2023/repos/DictionariesLambdaLINQExercise/05.StudentAcademy/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml.Linq;
+
+namespace _05.StudentAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> students = new();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (students.ContainsKey(name))
+                {
+                    students[name].Add(grade);
+                }
+                else
+                {
+                    students.Add(name, new List<double>());
+                    students[name].Add(grade);
+                }
+            }
+
+            foreach (KeyValuePair<string, List<double>> pair in students) 
+            {
+                double averageGrade = pair.Value.Average();
+
+                if (averageGrade >= 4.50)
+                {
+                    Console.WriteLine($"{pair.Key} -> {averageGrade:f2}");
+                }
+                
+            }
+        }
+    }
+}
