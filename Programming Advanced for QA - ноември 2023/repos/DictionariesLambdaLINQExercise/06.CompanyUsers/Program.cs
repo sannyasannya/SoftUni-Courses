4a8cd2f86cba54ab3d65a7392c12d3d8b16bc842
sannyasannya <sanya.vasileva@mail.bg>
2024-02-25 15:41:40 +0200

    Add new problems from Dictionaries, Lambda and LINQ.

---
+++ Programming Advanced for QA - ноември 2023/repos/DictionariesLambdaLINQExercise/06.CompanyUsers/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _06.CompanyUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End") 
+            {
+                string[] inputArray = input.Split(" -> ");
+                string company = inputArray[0];
+                string employeeID = inputArray[1];
+
+                if (!companies.ContainsKey(company))
+                {
+                    companies.Add(company, new List<string>());
+                }
+
+                if (!companies[company].Contains(employeeID))
+                {
+                    companies[company].Add(employeeID);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (KeyValuePair<string, List<string>> currentCompany in companies) 
+            {
+                Console.WriteLine(currentCompany.Key);
+
+                foreach (string currentCompanyID in currentCompany.Value)
+                {
+                    Console.WriteLine($"-- {currentCompanyID}");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Programming Advanced for QA - ноември 2023/repos/DictionariesLambdaLINQExercise/3.OrdersWith2Dictionaries/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _3.OrdersWith2Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, decimal> productPrice = new();
+            Dictionary<string, int> productQuantity = new();
+
+            string input = Console.ReadLine();
+
+            while (input != "buy")
+            {
+                string[] inputArray = input.Split();
+                string product = inputArray[0];
+                decimal price = decimal.Parse(inputArray[1]);
+                int quantity = int.Parse(inputArray[2]);
+
+                if (productPrice.ContainsKey(product))
+                {
+                    productPrice[product] = price;
+                    productQuantity[product] += quantity;
+                }
+                else
+                {
+                   productPrice.Add(product, price);
+                    productQuantity.Add(product, quantity);
+                }
+                input = Console.ReadLine();
+            }
+
+            foreach (string currentProduct in productPrice.Keys)
+            {
+                Console.WriteLine($"{currentProduct} -> " +
+                    $"{(productPrice[currentProduct] * productQuantity[currentProduct]):f2}");
+            }
+            
+        }
+    }
+}
