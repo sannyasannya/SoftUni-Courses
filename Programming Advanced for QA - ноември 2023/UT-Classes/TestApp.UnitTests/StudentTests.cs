70e370adc59a4fc347ce61955a0a86fddf3a893a
sannyasannya <sanya.vasileva@mail.bg>
2024-03-08 11:46:49 +0200

    Add new problems with Unit Tests from Objects and Classes.

---
+++ Programming Advanced for QA - ноември 2023/UT-Classes/TestApp.UnitTests/StudentTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class StudentTests
+{
+    private Student _student;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._student = new();
+    }
+    
+    [Test]
+    public void Test_AddAndGetByCity_ReturnsStudentsInCity_WhenCityExists()
+    {
+        // Arrange
+        //Student student = new Student();
+        string[] students = { "John Doe 25 Sofia", "Jane Smith 22 Varna", "Alice Johnson 20 Sofia" };
+        string expected = $"John Doe is 25 years old.{Environment.NewLine}Alice Johnson is 20 years old.";
+
+        // Act
+        string result = this._student.AddAndGetByCity(students, "Sofia");
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+        //Assert.AreEqual(expected,resul);
+    }
+
+    [Test]
+    public void Test_AddAndGetByCity_ReturnsEmptyString_WhenCityDoesNotExist()
+    {
+        // Arrange
+        string[] students = { "John Doe 25 Sofia", "Jane Smith 22 Varna", "Alice Johnson 20 Sofia" };
+
+        // Act
+        string result = this._student.AddAndGetByCity(students, "Belgrade");
+
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_AddAndGetByCity_ReturnsEmptyString_WhenNoStudentsGiven()
+    {
+        // Arrange
+        string[] students = {  };
+
+        // Act
+        string result = this._student.AddAndGetByCity(students, "Belgrade");
+
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+}
