628beb1b7ab392250635eb0503212b2d1e433a95
sannyasannya <sanya.vasileva@mail.bg>
2024-03-09 16:57:19 +0200

    Add new problems from Exceptions and Error Handling.

---
+++ Programming Advanced for QA - ноември 2023/repos/ExceptionsAndErrorHandlingLab/01.SquareRoot/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (num < 0)
+                {
+                    throw new ArgumentException();
+                }
+                double sqrt = Math.Sqrt(num);
+                Console.WriteLine(sqrt);
+            }
+            catch(ArgumentException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally 
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+        }
+    }
+}
