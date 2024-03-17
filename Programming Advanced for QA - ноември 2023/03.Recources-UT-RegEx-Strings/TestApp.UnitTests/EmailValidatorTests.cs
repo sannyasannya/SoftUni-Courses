b42080976f69f9e966c1c22694ad627e9da22285
sannyasannya <sanya.vasileva@mail.bg>
2024-03-17 08:07:29 +0200

    Add Unit Tests with RegEx - Strings.

---
+++ Programming Advanced for QA - ноември 2023/03.Recources-UT-RegEx-Strings/TestApp.UnitTests/EmailValidatorTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailValidatorTests
+{
+    [TestCase("ivan_ivanov@softuni.bg")]
+    [TestCase("petar.petrov@abv.bg")]
+    [TestCase("sanya+123@yahoo.com")]
+    public void Test_ValidEmails_ReturnsTrue(string email)
+    {
+        // Arrange
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+    
+    [TestCase("invalid mail@invalid.domain")]
+    [TestCase("valid%mail@in_valid.domain")]
+    [TestCase("invalid/mail@invalid.d")]
+    [TestCase("invalid/mailinvalid.d")]
+    public void Test_InvalidEmails_ReturnsFalse(string email)
+    {
+        // Arrange
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+        
+        // Assert
+        Assert.That(result, Is.False);
+    }
+}
