ada6e9ef26ddcf9716d04307bfe48ace741ef4a7
sannyasannya <sanya.vasileva@mail.bg>
2024-03-20 09:12:18 +0200

    Add new problems with Unit Tests from RegEx with Strings.

---
+++ Programming Advanced for QA - ноември 2023/03.Recources-UT-RegEx-Strings/TestApp.UnitTests/SubstringTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class SubstringTests
+{    
+    [Test]
+    public void Test_RemoveOccurrences_RemovesSubstringFromMiddle()
+    {
+        // Arrange
+        string toRemove = "fox";
+        string input = "The quick brown fox jumps over the lazy dog";
+
+        // Act
+        string result = Substring.RemoveOccurrences(toRemove, input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("The quick brown jumps over the lazy dog"));
+    }
+
+    [Test]
+    public void Test_RemoveOccurrences_RemovesSubstringFromStart()
+    {
+        // Arrange
+        string toRemove = "The";
+        string input = "The quick brown fox jumps over a lazy dog";
+
+        // Act
+        string result = Substring.RemoveOccurrences(toRemove, input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("quick brown fox jumps over a lazy dog"));
+    }
+
+    [Test]
+    public void Test_RemoveOccurrences_RemovesSubstringFromEnd()
+    {
+        // Arrange
+        string toRemove = "dog";
+        string input = "The quick brown fox jumps over the lazy dog";
+
+        // Act
+        string result = Substring.RemoveOccurrences(toRemove, input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("The quick brown fox jumps over the lazy"));
+    }
+
+    [Test]
+    public void Test_RemoveOccurrences_RemovesAllOccurrences()
+    {
+        // Arrange
+        string toRemove = "the";
+        string input = "the the the the the";
+
+        // Act
+        string result = Substring.RemoveOccurrences(toRemove, input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(""));
+    }
+}
