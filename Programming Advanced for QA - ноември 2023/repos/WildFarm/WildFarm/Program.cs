74c3e39e7ee548713828fef0bcdb3b4e97a43ced
sannyasannya <sanya.vasileva@mail.bg>
2024-03-26 16:49:48 +0200

    Add a new problem from OOP.

---
+++ Programming Advanced for QA - ноември 2023/repos/WildFarm/WildFarm/Program.cs
@@ -0,0 +1,42 @@
+﻿using WildFarm.Animals;
+using WildFarm.Animals.Mammals.Felines;
+using WildFarm.Foods;
+
+namespace WildFarm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Farm farm = new Farm();
+
+            string animalInput = Console.ReadLine();
+
+            while (animalInput != "End")
+            {
+                Animal animal = farm.ReadAnimal(animalInput);
+                farm.AddAnimal(animal);
+                Console.WriteLine(animal.Talk());
+
+                string foodInput = Console.ReadLine();
+                Food food = farm.ReadFood(foodInput);
+
+                try
+                {                    
+                    farm.FeedAnimal(animal, food);
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine($"{animal.GetType().Name} does not eat {food.GetType().Name}!");
+                }                
+
+                animalInput = Console.ReadLine();
+            }   
+
+            foreach (Animal currentAnimal in farm.Animals)
+            {
+                Console.WriteLine(currentAnimal);
+            }
+        }
+    }
+}
