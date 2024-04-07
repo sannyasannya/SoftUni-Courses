cb359bb39c606a9baf7cf1a64ad847f86df4506c
sannyasannya <sanya.vasileva@mail.bg>
2024-04-07 16:41:57 +0300

    Add new problems wit Unit Tests with Dictionaries from Programming Advanced for QA.

---
+++ Programming Advanced for QA - ноември 2023/Exam-Preparation-1/Resources/02-Dictionary-Insertion-Resources/TestApp.Tests/DictionaryIntersectionTests.cs
@@ -0,0 +1,121 @@
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+[TestFixture]
+public class DictionaryIntersectionTests
+{
+    [Test]
+    public void Test_Intersect_TwoEmptyDictionaries_ReturnsEmptyDictionary()
+    {
+        // Arrange
+        Dictionary<string, int> firstDictionary = new();
+        Dictionary<string, int> secondDictionary = new();
+
+        // Act
+        Dictionary<string, int> result = DictionaryIntersection.Intersect(firstDictionary, secondDictionary);
+
+        // Assert       
+        Assert.That(result, Has.Count.EqualTo(0));
+    }
+
+    [Test]
+    public void Test_Intersect_OneEmptyDictionaryAndOneNonEmptyDictionary_ReturnsEmptyDictionary()
+    {
+        // Arrange
+        Dictionary<string, int> firstDictionary = new()
+        {
+            {"one", 1},
+            {"two", 2}
+        };
+
+        Dictionary<string, int> secondDictionary = new();
+
+        // Act
+        Dictionary<string, int> result = DictionaryIntersection.Intersect(firstDictionary, secondDictionary);
+
+        // Assert       
+        Assert.That(result, Has.Count.EqualTo(0));
+    }
+
+    [Test]
+    public void Test_Intersect_TwoNonEmptyDictionariesWithNoCommonKeys_ReturnsEmptyDictionary()
+    {
+        // Arrange
+        Dictionary<string, int> firstDictionary = new()
+        {
+            {"one", 1},
+            {"two", 2}
+        };
+
+        Dictionary<string, int> secondDictionary = new()
+        {
+            { "three", 3 },
+            { "four", 4 }
+        };
+
+        // Act
+        Dictionary<string, int> result = DictionaryIntersection.Intersect(firstDictionary, secondDictionary);
+
+        // Assert       
+        Assert.That(result, Has.Count.EqualTo(0));
+    }
+
+    [Test]
+    public void Test_Intersect_TwoNonEmptyDictionariesWithCommonKeysAndValues_ReturnsIntersectionDictionary()
+    {
+        // Arrange
+        Dictionary<string, int> firstDictionary = new()
+        {
+            {"one", 1},
+            {"two", 2}
+        };
+
+        Dictionary<string, int> secondDictionary = new()
+        {
+            { "one", 1 },
+            { "two", 2 },
+            { "four", 4 }
+        };
+
+        // Act
+        Dictionary<string, int> result = DictionaryIntersection.Intersect(firstDictionary, secondDictionary);
+
+        // Assert       
+        Assert.AreEqual(2, result.Count);
+
+        Assert.IsTrue(result.ContainsKey("one"));
+        Assert.AreEqual(1, result["one"]);
+
+        Assert.IsTrue(result.ContainsKey("two"));
+        Assert.AreEqual(2, result["two"]);
+
+        Assert.IsFalse(result.ContainsKey("four"));
+    }
+
+    [Test]
+    public void Test_Intersect_TwoNonEmptyDictionariesWithCommonKeysAndDifferentValues_ReturnsEmptyDictionary()
+    {
+        // Arrange
+        Dictionary<string, int> firstDictionary = new()
+        {
+            {"one", 1},
+            {"two", 2}
+        };
+
+        Dictionary<string, int> secondDictionary = new()
+        {
+            { "one", 10 },
+            { "two", 20 }
+        };
+
+        // Act
+        Dictionary<string, int> result = DictionaryIntersection.Intersect(firstDictionary, secondDictionary);
+
+        // Assert
+        Assert.AreEqual(0, result.Count);
+        Assert.IsTrue(!result.Any());
+    }
+}
