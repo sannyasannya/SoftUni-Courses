33650202280f358e5cdc0146416b7b53767cd1f4
sannyasannya <sanya.vasileva@mail.bg>
2024-04-26 20:57:42 +0300

    Add new problems with JS-Basics from Back-End Technologies Basics.

---
+++ Back-End Technologies Basics - януари 2024/JS-Basics/JS-Lab-Problems/00.Demo.js
@@ -0,0 +1,11 @@
+//function solve(name, grade){
+  //  console.log(`My name is: ${name}, my grade is: ${grade.toFixed(2)}`);
+//}
+// solve('Sanya', 5.5265);
+
+
+//let test;
+//console.log(test)
+
+let test1 = 'Hello';
+console.log(typeof test1)
---
+++ Back-End Technologies Basics - януари 2024/JS-Basics/JS-Lab-Problems/03.excellent-grade.js
@@ -0,0 +1,15 @@
+function solve(grade) {
+    'use strict';
+
+    if(grade >= 5.50) {
+        console.log("Excellent");
+    } else {
+        console.log("Not excellent");
+    }
+}
+
+//const gradeName = grade >= 5.50 ? 'Excellent' : 'Not excellent';
+//console.log(gradeName);
+
+
+solve(4.35);
