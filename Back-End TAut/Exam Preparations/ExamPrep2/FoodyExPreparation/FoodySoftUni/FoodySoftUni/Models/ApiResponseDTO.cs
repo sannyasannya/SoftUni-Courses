7298f929b513c419c53e4e27a1fd4df0c1efd9db
sannyasannya <sanya.vasileva@mail.bg>
2024-07-09 18:35:50 +0300

    Exam Preparation 2 from Back-End Test Automation.

---
+++ Back-End TAut/Exam Preparations/ExamPrep2/FoodyExPreparation/FoodySoftUni/FoodySoftUni/Models/ApiResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FoodySoftUni.Models
+{
+    public class ApiResponseDTO
+    {
+        [JsonPropertyName("msg")]
+        public string Message { get; set; }
+
+        [JsonPropertyName("foodId")]
+        public string? FoodId { get; set; }
+    }
+}
