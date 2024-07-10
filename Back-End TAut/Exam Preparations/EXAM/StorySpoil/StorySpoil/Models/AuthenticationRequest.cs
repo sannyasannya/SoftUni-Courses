39ef0558e007cca19338775ed05cde59daf097b1
sannyasannya <sanya.vasileva@mail.bg>
2024-07-10 17:56:42 +0300

    Regular Exam from Back-End Test Automation.

---
+++ Back-End TAut/Exam Preparations/EXAM/StorySpoil/StorySpoil/Models/AuthenticationRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace StorySpoil.Models
+{
+    public class AuthenticationRequest
+    {
+        [JsonPropertyName("userName")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+    }
+}
