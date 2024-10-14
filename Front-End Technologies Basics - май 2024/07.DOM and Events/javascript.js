9018c6c2697851c626ce6cd8c47ef7ec71edf983
sannyasannya <sanya.vasileva@mail.bg>
2024-10-14 11:48:00 +0300

    Add DOM and Events - Lab.

---
+++ Front-End Technologies Basics - май 2024/07.DOM and Events/javascript.js
@@ -0,0 +1,13 @@
+//let div = document.getElementById("div1");
+//console.log(div.innerHTML);
+//div.innerHTML = "<a>Click here</a>"
+
+//div.style.background = "blue";
+
+
+//let input = document.querySelector("input");
+//input.value = "Please add text";
+
+
+let input = document.querySelector("input");
+input.id = "textInput";
